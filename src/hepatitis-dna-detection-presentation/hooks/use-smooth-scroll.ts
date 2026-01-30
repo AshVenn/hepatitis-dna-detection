f8dc70a675ae77d5a14ad51abcae4313d9b76f32
Youness Zouhairi <sora7sk@gmail.com>
2026-01-30 22:02:54 +0100

    Fix Lenis options: remove unsupported smoothTouch

--- src/hepatitis-dna-detection-presentation/hooks/use-smooth-scroll.ts
+++ src/hepatitis-dna-detection-presentation/hooks/use-smooth-scroll.ts
@@ -16,7 +16,6 @@ export function useSmoothScroll() {
         gestureOrientation: 'vertical',
         smoothWheel: true,
         wheelMultiplier: 1,
-        smoothTouch: false,
         touchMultiplier: 2,
         infinite: false,
       })
